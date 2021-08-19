e29fed302f2ea0066a91b86587881bf07f66c6b5
HitmanCodes <72141209+HitmanCodes@users.noreply.github.com>
2021-08-19 22:42:32 +0530

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import StudentView,welcome,GetStudent
+from .views import StudentView,welcome,GetStudent,Getstudentdetails
 
 urlpatterns=[
     path('',welcome),
     path('getstudent',StudentView.as_view()),
     path('verifystudent',GetStudent.as_view()),
-    path('getstudentinfo',getstudentdetails.as_view())
+    path('getstudentinfo',Getstudentdetails.as_view())
 ]
