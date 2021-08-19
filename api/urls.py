1ac52c097b1111ea0e02d5c6ee1846b4b34b1342
Harsh Vardhan omar <harshvardhan181201@gmail.com>
2021-08-19 21:34:05 +0530

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -5,4 +5,5 @@ urlpatterns=[
     path('',welcome),
     path('getstudent',StudentView.as_view()),
     path('verifystudent',GetStudent.as_view()),
+    path('getstudentinfo',getstudentdetails.as_view())
 ]
