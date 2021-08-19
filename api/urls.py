a6ff88be062a215bf8b035e33a4ab2e547fa1546
Harsh Vardhan omar <harshvardhan181201@gmail.com>
2021-08-19 22:59:06 +0530

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import StudentView,welcome,GetStudent,Getstudentdetails
+from .views import StudentView,welcome,GetStudent
 
 urlpatterns=[
     path('',welcome),
     path('getstudent',StudentView.as_view()),
     path('verifystudent',GetStudent.as_view()),
-    path('getstudentinfo',Getstudentdetails.as_view())
 ]
 
