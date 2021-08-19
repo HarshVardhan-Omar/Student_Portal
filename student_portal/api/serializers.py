04002ec5745bcb915d969e8ac4d868fd431c120a
Harsh Vardhan omar <harshvardhan181201@gmail.com>
2021-08-19 12:24:22 +0530

    Update api/serializers.py

--- student_portal/api/serializers.py
+++ student_portal/api/serializers.py
@@ -5,8 +5,3 @@ class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model=Student
         fields=('id','user_name','password')
-
-class StudentVerifySerializer(serializers.ModelSerializer):
-    class Meta:
-        model=Student
-        fields=('user_name','password')
