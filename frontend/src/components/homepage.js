8087c88247e4cc98b931ca28cbe77473ccadd01c
Harsh Vardhan omar <harshvardhan181201@gmail.com>
2021-08-19 23:03:20 +0530

    Update homepage.js

--- frontend/src/components/homepage.js
+++ frontend/src/components/homepage.js
@@ -1,29 +1,15 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
-import { useState } from 'react';
 
 export default function homepage() {
     const location=useLocation();
-    const[user_name,setUser_Name]=useState("")
-    const[password,setPassword]=useState("")
-    const getstudentdetails=()=>{
-        fetch("/api/getstudentinfo?credentials=" + location.state.username + "::" + location.state.password)
-        .then((response)=>{
-            return  response.json()
-        }).then((data)=>{
-            setUser_Name(data.username)
-            setPassword(data.password)
 
-        }).catch((err)=>{
-            console.log(err)
-        })
-    }
-    getstudentdetails();
+    
     return (
         <div>
             <h1>Welcome to Homepage</h1>
-            <h2>The following user just signed in with username- {user_name} and
-             password -{password}</h2>
+            <h2>The following user just signed in with username- {location.state.data.username} and
+             password -{location.state.data.password}</h2>
         </div>
     )
 }
