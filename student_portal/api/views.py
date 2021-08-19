8cfcb88561e4c1e2360cfe637170fa188b77fd74
Harsh Vardhan omar <harshvardhan181201@gmail.com>
2021-08-19 12:21:02 +0530

    Update api/views.py

--- student_portal/api/views.py
+++ student_portal/api/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import HttpResponse
 from rest_framework import generics,status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .serializers import StudentSerializer,StudentVerifySerializer
+from .serializers import StudentSerializer
 from .models import Student
 
 # Create your views here.
 
 def welcome(request):
-    return HttpResponse("<h1>Welcome To Backend</h1>")
+    return HttpResponse("<h1>bsdk tm hamare ghar me aa gaye Nikal Laude</h1>")
 
 
 class StudentView(generics.ListAPIView):
@@ -16,33 +16,25 @@ class StudentView(generics.ListAPIView):
     serializer_class=StudentSerializer
 
 class GetStudent(APIView):
+    def get(self, request,format=None):
+        return HttpResponse("<h1>BSDK Aukat me madarchod tmhare baap ki api ni hai</h1>")
+    serializer_class=StudentSerializer
     lookup_url_kwarg='user_name'
     lookup_url_kwarg_pass='password'
     def post(self,request,format=None):
         user_name=request.data.get(self.lookup_url_kwarg)
         password=request.data.get(self.lookup_url_kwarg_pass)
         if user_name!=None and password!=None:
+            # print(user_name)
+            # print(password)
             student=Student.objects.filter(user_name=user_name)
-            student1=Student.objects.filter(password=password)
-            if len(student)>0 and len(student1)>0:
-                data=student[0]
-                return Response({'User Authenticated'},status=status.HTTP_200_OK)
-            return Response({'Bad Request:','User Does not exist'},status=status.HTTP_400_BAD_REQUEST)
-        return Response({'Bad Request:','Request parameter Not meant'},status=status.HTTP_401_UNAUTHORIZED)
+            if len(student)>0:
+                # print(self.serializer_class(student[0]).data["password"])
+                if password == self.serializer_class(student[0]).data["password"]:
+                    datum = self.serializer_class(student[0]).data
+                    return Response(datum,status=status.HTTP_200_OK)
+                return Response({'Incorrect Password'},status=status.HTTP_401_UNAUTHORIZED)
+            return Response({'Bad Request:','User Does not exist'},status=status.HTTP_204_NO_CONTENT)
+        return Response({'Bad Request:','Request parameter Not meant'},status=status.HTTP_400_BAD_REQUEST)
 
-
-class GetStudentView(APIView):
-    serializer_class=StudentVerifySerializer
-
-    def post(self,request,formamt=None):
-        serializer=self.serializer_class(data=request.data)
-        if serializer.is_valid():
-            user_name=serializer.data.get('user_name')
-            password=serializer.data.get('password')
-            queryset=Student.objects.filter(user_name=user_name)
-            if queryset.exists():
-                student=queryset[0]
-                return Response(StudentSerializer(student).data,status=status.HTTP_200_OK)
-            else:
-                return Response({'Bad Request:','User Does Not Exist'},status=status.HTTP_400_BAD_REQUEST)
 
