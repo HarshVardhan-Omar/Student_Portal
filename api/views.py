b2ef26b3910c76e150743a8b418c8f21fe67e2b4
HitmanCodes <72141209+HitmanCodes@users.noreply.github.com>
2021-08-29 20:22:51 +0530

    ADDED PROPER FRONTEND
    Login Page Added

---
+++ api/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import HttpResponse
+from rest_framework import generics,status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import StudentSerializer
+from .models import Student
+
+# Create your views here.
+
+def welcome(request):
+    return HttpResponse("<h1>bsdk tm hamare ghar me aa gaye Nikal Laude</h1>")
+
+
+class StudentView(generics.ListCreateAPIView):
+    queryset=Student.objects.all()
+    serializer_class=StudentSerializer
+
+    
+class GetStudent(APIView):
+    def get(self, request,format=None):
+        return HttpResponse("<h1>BSDK Aukat me madarchod tmhare baap ki api ni hai</h1>")
+    serializer_class=StudentSerializer
+    lookup_url_kwarg_user='user_name'
+    lookup_url_kwarg_pass='password'
+    def post(self,request,format=None):
+        username=request.data.get(self.lookup_url_kwarg_user)
+        password=request.data.get(self.lookup_url_kwarg_pass)
+        if username!=None and password!=None:
+            student=Student.objects.filter(username=username)
+            if len(student)>0:
+                if password == self.serializer_class(student[0]).data["password"]:
+                    datum = self.serializer_class(student[0]).data
+                    return Response(datum, status=status.HTTP_200_OK)
+                return Response({'Incorrect Password'},status=status.HTTP_401_UNAUTHORIZED)
+            return Response({'Bad Request:','User Does not exist'},status=status.HTTP_404_NOT_FOUND)
+        return Response({'Bad Request:','Request parameter Not meant'},status=status.HTTP_400_BAD_REQUEST)
---
+++ frontend/src/components/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { component, useState } from "react"
+import LoadingBar from 'react-top-loading-bar'
+import LoginPage from "./loginPage";
+import homepage from "./homepage";
+import { BrowserRouter as Router,Switch,Route } from "react-router-dom";
+
+export default function App() {
+
+  const[progress,setProgress]=useState(0);
+
+  return (
+    <>
+    <LoadingBar color='#c8ff93' shadow={true} height={3} progress={progress} onLoaderFinished={() => setProgress(0)}/>
+    <Router>
+      <Switch>
+        <Route exact path="/"> <LoginPage setProgress={setProgress} /> </Route>
+        <Route exact path="/getstudentdetails"component={homepage} />
+      </Switch>
+    </Router>
+    </>
+  );
+}
+const app = document.getElementById("app");
+ReactDOM.render(<App />, app);
