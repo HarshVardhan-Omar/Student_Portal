7a524999c6654f6b642199d04d2d88826172b64b
Harsh Vardhan omar <harshvardhan181201@gmail.com>
2021-08-29 13:10:30 +0530

    Updated Models

--- api/models.py
+++ api/models.py
@@ -5,3 +5,5 @@ from django.db import models
 class Student(models.Model):
     username=models.TextField(max_length=50, default="",null=False,unique=True)
     password=models.TextField(max_length=40,null=False,unique=False)
+    name=models.CharField(max_length=50,default="",null=False,unique=False)
+    Nationality=models.CharField(max_length=10,default="",null=False)
