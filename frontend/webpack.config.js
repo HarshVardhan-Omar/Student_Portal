8a66377d554f0f1d492ad29863951074582edd75
Harsh Vardhan omar <harshvardhan181201@gmail.com>
2021-08-29 14:31:43 +0530

    Added CSS and File loader to Webpack

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -16,6 +16,18 @@ module.exports = {
           loader: "babel-loader",
         },
       },
+        {
+          test: /\.(png|jpe?g|gif)$/i,
+          use: [
+            {
+              loader: 'file-loader',
+            },
+          ]
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
     ],
   },
   optimization: {
