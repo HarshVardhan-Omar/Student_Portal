b00ef1a2ef4fd9de4417bb21feee9508a3b5dabb
HitmanCodes <72141209+HitmanCodes@users.noreply.github.com>
2021-09-07 08:11:47 +0530

    Delete frontend directory

--- frontend/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import index,icon
-
-
-urlpatterns=[
-    path('',index,name="Home Page Path"),
-    path('getstudentdetails',index),
-    path('favicon.ico',icon)
-]
