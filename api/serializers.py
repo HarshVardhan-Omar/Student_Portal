0aa9ab0fade896f1d5e40de1e05777daf90f85e5
HitmanCodes <72141209+HitmanCodes@users.noreply.github.com>
2021-09-07 07:41:44 +0530

    added serializers for session storage model

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import Student
+from .models import Student,StoredSessions
 
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model=Student
         fields="__all__"
+
+class SessionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=StoredSessions
+        fields=["username","key"]
