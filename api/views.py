4abaa1a38f52ad2550194631f2671d092e1db8ba
HitmanCodes <72141209+HitmanCodes@users.noreply.github.com>
2021-09-07 08:17:23 +0530

    adding views for session storage and deletion

--- api/views.py
+++ api/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import HttpResponse
 from rest_framework import generics,status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .serializers import StudentSerializer
-from .models import Student
+from .serializers import StudentSerializer,SessionSerializer
+from .models import Student,StoredSessions
 
 # Create your views here.
 
@@ -23,14 +23,55 @@ class GetStudent(APIView):
     lookup_url_kwarg_user='user_name'
     lookup_url_kwarg_pass='password'
     def post(self,request,format=None):
+
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+
         username=request.data.get(self.lookup_url_kwarg_user)
         password=request.data.get(self.lookup_url_kwarg_pass)
         if username!=None and password!=None:
             student=Student.objects.filter(username=username)
-            if len(student)>0:
+            if len(student)==1:
                 if password == self.serializer_class(student[0]).data["password"]:
+                    sessionobject = StoredSessions(key=self.request.session.session_key, username=username)        
+                    sessionobject.save()
                     datum = self.serializer_class(student[0]).data
                     return Response(datum, status=status.HTTP_200_OK)
                 return Response({'Incorrect Password'},status=status.HTTP_401_UNAUTHORIZED)
             return Response({'Bad Request:','User Does not exist'},status=status.HTTP_404_NOT_FOUND)
         return Response({'Bad Request:','Request parameter Not meant'},status=status.HTTP_400_BAD_REQUEST)
+
+
+class GetStudentBySession(APIView):
+    def get(self, request,format=None):
+        return HttpResponse("<h1>BSDK Aukat me madarchod tmhare baap ki api ni hai</h1>")
+    serializer_class=StudentSerializer
+    session_serializer_class=SessionSerializer
+    def post(self,request,format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+        sessionobject = StoredSessions.objects.filter(key=self.request.session.session_key)
+        if len(sessionobject)==1:
+            username=self.session_serializer_class(sessionobject[0]).data["username"]
+            student=Student.objects.filter(username=username)
+            datum = self.serializer_class(student[0]).data
+            return Response(datum, status=status.HTTP_200_OK)
+        return Response({'Login Required:','No Session Found'},status=status.HTTP_204_NO_CONTENT)
+
+class LogoutStudentBySession(APIView):
+    def get(self, request,format=None):
+        return HttpResponse("<h1>BSDK Aukat me madarchod tmhare baap ki api ni hai</h1>")
+    serializer_class=StudentSerializer
+    def post(self,request,format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+        sessionobject = StoredSessions.objects.filter(key=self.request.session.session_key)
+        if sessionobject.exists():
+            sessionfound = sessionobject[0]
+            sessionfound.delete();
+            return Response({'OK:','OK'}, status=status.HTTP_200_OK)
+        return Response({'Login Required:','No Session Found'},status=status.HTTP_204_NO_CONTENT)
+
+class SessionsView(generics.ListAPIView):
+    queryset=StoredSessions.objects.all()
+    serializer_class=SessionSerializer
