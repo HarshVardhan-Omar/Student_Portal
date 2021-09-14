04bd747e6a99ad42f6ac11706e90ce46d7cc790e
HitmanCodes <72141209+HitmanCodes@users.noreply.github.com>
2021-09-14 12:51:22 +0530

    demo changepassword component

---
+++ frontend/src/components/ChangePassword.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ChangePassword(props) {
+    return (
+        <div>
+            <h1>Hello {props.Name}</h1>
+            <h1>This Is ChangePassword Page</h1>
+        </div>
+    )
+}
+
+export default ChangePassword
