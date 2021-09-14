625931e5d7648e3a5decdb3c091dc7679a6c7f7b
HitmanCodes <72141209+HitmanCodes@users.noreply.github.com>
2021-09-14 12:06:46 +0530

    changed method of data showing
    from location state to use State

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -9,14 +9,15 @@ import { BrowserRouter as Router,Switch,Route } from "react-router-dom";
 export default function App() {
 
   const[progress,setProgress]=useState(0);
+  const[data,setData]=useState();
 
   return (
     <>
     <LoadingBar color='#c8ff93' shadow={true} height={3} progress={progress} onLoaderFinished={() => setProgress(0)}/>
     <Router>
       <Switch>
-        <Route exact path="/" render={props => <LoginPage setProgress={setProgress} />} />
-        <Route path="/getstudentdetails" render={props => <Homepage setProgress={setProgress} />}  />
+        <Route exact path="/" render={props => <LoginPage setData={setData} setProgress={setProgress} />} />
+        <Route path="/getstudentdetails" render={props => <Homepage setData={setData} data={data} setProgress={setProgress} />}  />
         <Route> <h1> 404 Not Found. </h1></Route>
       </Switch>
     </Router>
