e4a142164951e21a2f5ca6db3291b63953a3cc5b
YASH VARDHAN <72141209+HitmanCodes@users.noreply.github.com>
2021-09-21 00:19:20 +0530

    added anchor for going back to login/home

--- frontend/src/components/NotFound.js
+++ frontend/src/components/NotFound.js
@@ -9,7 +9,8 @@ function NotFound() {
                     <h3>Oops!</h3>
                     <h1><span>#</span><span>4</span><span>0</span><span>4</span></h1>
                 </div>
-                <h2>We are sorry, the page you requested was not found</h2>
+                <h2>We are sorry, the page you requested was not found.</h2>
+                <a className="errgobackbutn" style={{ fontFamily: "'Cabin', sans-serif"}} href="/">Get Back!!</a>
             </div>
         </div>
     );
