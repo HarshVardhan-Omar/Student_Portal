ce19c4c829433a9ac31fadb1f6cbcaf7c2f43d35
SIMARPRIT VIRDI <singhsv3699@gmail.com>
2021-09-20 20:56:15 +0530

    Error Page 404 Inserted

--- frontend/src/components/NotFound.js
+++ frontend/src/components/NotFound.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
+import "./error.css";
 
 function NotFound() {
     return (
-        <div>
-            Not Found
+        <div id="notfound">
+            <div className="notfound">
+                <div className="notfound-404">
+                    <h3>Oops!</h3>
+                    <h1><span>#</span><span>4</span><span>0</span><span>4</span></h1>
+                </div>
+                <h2>We are sorry, the page you requested was not found</h2>
+            </div>
         </div>
-    )
+    );
 }
-
-export default NotFound
+export default NotFound;
