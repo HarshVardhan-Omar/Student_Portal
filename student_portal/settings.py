6107a0659adc7a014d61f2d4c687b7c0af076445
HitmanCodes <72141209+HitmanCodes@users.noreply.github.com>
2021-09-20 20:39:40 +0530

    Updating Settings.py for displaying Custom 404

--- student_portal/settings.py
+++ student_portal/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2(#yyec9r1ku!a84wv*74+5hm+qavrg5n(qew&!c4as1+d5leg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT=os.path.join(BASE_DIR,'frontend/static')
 
 # Managing Media Directory
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
