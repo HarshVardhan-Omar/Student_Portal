77926d1ac952ed2d526a03d3489377c3abd90d93
HitmanCodes <72141209+HitmanCodes@users.noreply.github.com>
2021-09-20 20:45:37 +0530

    updating urls to display custom 404 page
    added local path(in settings.py too) for static files serving in false debug mode
    in deployment the location in settings.py for static root is the hosting server address followed by a collectstatic command

--- student_portal/urls.py
+++ student_portal/urls.py
@@ -17,9 +17,14 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.urls.conf import re_path
+from django.views.static import serve
 urlpatterns = [
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
     path('admin/', admin.site.urls),
     path('',include('frontend.urls')),
     path('api/',include('api.urls')),
 ]   + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+handler404 = "frontend.views.not_found"
