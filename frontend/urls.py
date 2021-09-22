2ba9b867d60c680b88cdcc8b65357e1fa1d8ac98
YASH VARDHAN <72141209+HitmanCodes@users.noreply.github.com>
2021-09-22 15:36:54 +0530

    updating urls.py to refactor match

--- frontend/urls.py
+++ frontend/urls.py
@@ -4,6 +4,7 @@ from .views import index,icon
 
 urlpatterns=[
     path('',index,name="Home Page Path"),
-    path('getstudentdetails',index),
+    path('getstudentdetails/',index),
+    path('getstudentdetails/<str:nextpath>',index),
     path('favicon.ico',icon)
 ]
