e23fbad4bdd1a86ca72a7e602d153224216542d4
Harsh Vardhan omar <harshvardhan181201@gmail.com>
2022-12-09 11:58:46 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 echo "BUILD START"
-python3.11.1 -m pip install -r requirements.txt
-python3.11.1 manage.py collectstatic --noinput --clear
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput --clear
 echo "BUILD END"
